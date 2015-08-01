4cbc93f40481a29f8b4f51e7e84a5ab8000913a0
Erick Florez <erick.florez@gmail.com>
2015-08-01 10:23:54 -0500

    MDL Bare Bones Starter Kit
    This is a bare bones base starter kit that uses the Material Design Lite
    Framework

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// npm install gulp-jshint gulp-sass gulp-concat gulp-uglify gulp-rename --save-dev
+
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('assets/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('assets/scss/**/**/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/css'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('assets/js/*.js', ['lint', 'scripts']);
+    gulp.watch('assets/scss/**/**/**/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'watch']);
